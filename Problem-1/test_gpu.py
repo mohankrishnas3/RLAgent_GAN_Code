fc8d0f2f59673d52804e8ad64f2160b16cfeff48
Mohan Krishna Sunkara <smohankrishna95@gmail.com>
2025-10-01 16:24:23 -0400

    Test_GPU

---
+++ Problem-1/test_gpu.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+# Check for GPU devices
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(f"✅ Found {len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+else:
+    print("❌ No GPU found. TensorFlow will run on CPU.")
